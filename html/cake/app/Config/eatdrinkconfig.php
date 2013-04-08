80a12737e484da9f868c374b7bae8d90a4d4efb3
Kyle Zinter <kylezinter@Kyles-MacBook-Pro.local>
2013-04-08 02:19:49 -0700

    Config updates

--- html/cake/app/Config/eatdrinkconfig.php
+++ html/cake/app/Config/eatdrinkconfig.php
@@ -1,3 +1,4 @@
 <?php
-	$config['connection'] = array('spotsdb' => '<insert key>',);
+	$config['connection'] = array('spotsdb' => 'https://api.mongolab.com/api/1/databases/eatdrinksea/collections/Spots?s={"likes": -1, "name": 1}&apiKey={INSERTKEYHERE}',);
+	$config['apiKey'] = array('apiKey' => '{INSERTKEYHERE}');
 ?>
