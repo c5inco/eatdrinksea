a61f6394402e39caf147b01872cadb82e2166155
Kyle Zinter <kylezinter@Kyles-MacBook-Pro-2.local>
2013-05-19 15:28:39 -0700

    config issues, delete, will re-add

--- html/cake/app/Config/eatdrinkconfig.php
+++
@@ -1,7 +0,0 @@
-<?php    
-    $config['connection'] = array(
-        'apiKey' => '<insert key>',
-        'spots-db' => 'https://api.mongolab.com/api/1/databases/eatdrinksea/collections/Spots?apiKey=<insert key>',
-        'run-cmd' => 'https://api.mongolab.com/api/1/databases/eatdrinksea/runCommand?apiKey=<insert key>'
-    );    
-?>
